84c29779c59785d4009e697777fd9e4474da81f0
suk3952111 <suk3952111@naver.com>
2024-11-04 17:46:00 +0900

    refactor: env 추가 및 프로젝트 이름 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
-
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vwextkqnwkxzzjvkqoqt.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3ZXh0a3Fud2t4enpqdmtxb3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4NTI1NzEsImV4cCI6MjAzMzQyODU3MX0.hDrkMW7BuWuQPSpvRuOoirHOp2VTwS3xo0a167mSolc";
+
+// 환경 변수에서 Supabase URL과 키를 가져옵니다.
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log(supabaseUrl);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
